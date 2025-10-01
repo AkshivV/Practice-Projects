ba3fc591fa11013dc7eb60e924371eec5046c36b
AkshivV <akshiv.verma@gmail.com>
2025-10-01 16:59:14 +0930

    completed simple madlibs project and modified Readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Projects
+
+**Regular projects to get hands on experience**
+
+
+
+## Beginner
+- *Madlibs* - Simple program that accepts input and uses and f-string to implant them into an existing sentence
+-  
---
+++ madlibs/madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+person = input("Person: ")
+
+print(f"Life is so {adj}. We get to do so much, some things i enjoy are {verb1} and {verb2}. That reminds me, I'd love to do {verb2} with {person}")
